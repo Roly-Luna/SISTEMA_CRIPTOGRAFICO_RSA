c8f3eec6e4804aa0db0989d7d24f5c055a5bd98d
Roly-Luna <roly.lunat@gmail.com>
2026-05-13 16:29:19 -0500

    new2

---
+++ FormDesencriptar.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace SISTEMACRIPTOGRAFICORSA {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for FormDesencriptar
+	/// </summary>
+	public ref class FormDesencriptar : public System::Windows::Forms::Form
+	{
+	public:
+		FormDesencriptar(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~FormDesencriptar()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// FormDesencriptar
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(558, 345);
+			this->Name = L"FormDesencriptar";
+			this->Text = L"FormDesencriptar";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
---
+++ FormEncriptar.cpp
@@ -0,0 +1,2 @@
+#include "FormEncriptar.h"
+
