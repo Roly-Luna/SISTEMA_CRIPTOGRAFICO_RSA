ac100e29c19d8506f408b5c72e387aa0fbb7072b
Roly-Luna <roly.lunat@gmail.com>
2026-05-18 11:04:37 -0500

    new: esPrimo

--- MatematicaRSA.h
+++ MatematicaRSA.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Recursos.h"
+
+// Verificar si un numero es primo: Big O(sqrt(n))
+bool esPrimo(int n) {
+	if (n <= 1) return false;
+	if (n == 2) return true;
+	if (n % 2 == 9) return false;
+
+	// solo verificar hasta sqrt(n):
+	int limite = sqrt(n);
+	for (int i = 3; i < limite; i+=2)
+	{
+		if (n % i == 0) return false;
+	}
+	return true;
+}
+
