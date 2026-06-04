214c31a52fc96b29416d08789cbd028310a96f5e
Roly-Luna <roly.lunat@gmail.com>
2026-06-04 18:35:19 -0500

    login

--- FormLogin.cpp
+++ FormLogin.cpp
@@ -1,2 +1,22 @@
+#include "FormPrincipal.h"
 #include "FormLogin.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[System::STAThreadAttribute]
+int main(cli::array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Iniciamos primero el formulario de Login
+    SISTEMACRIPTOGRAFICORSA::FormLogin login;
+
+    // Si el login responde que todo esta "OK" (contraseña correcta)...
+    if (login.ShowDialog() == DialogResult::OK) {
+        SISTEMACRIPTOGRAFICORSA::FormPrincipal formMain;
+        Application::Run(% formMain);
+    }
+
+    return 0;
+}
--- FormPrincipal.cpp
+++ FormPrincipal.cpp
@@ -1,17 +1,1 @@
-#include "FormPrincipal.h"
-
-using namespace System;
-using namespace System::Windows::Forms;
-
-[System::STAThreadAttribute]
-// agregamos cli:: a array
-int main(cli::array<String^>^ args) {
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-
-    // Inicia el formulario principal
-    SISTEMACRIPTOGRAFICORSA::FormPrincipal form;
-    Application::Run(% form);
-
-    return 0;
-}
+// #include "FormPrincipal.h"
